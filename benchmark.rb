009006574eedcce144f45b232934ecb02d0ad74d
Bradley Buda <bradleybuda@gmail.com>
2016-01-20 12:17:35 -0800

    Small refactor

--- benchmark.rb
+++ benchmark.rb
@@ -6,13 +6,6 @@ require 'uglifier'
 require 'terminal-table'
 require 'zlib'
 
-def percent_difference(baseline, measured, labels)
-  value = (measured.to_f - baseline.to_f) / baseline.to_f
-  label = (value < 0) ? labels.first : labels.last
-  rounded = (value.abs * 100).to_i
-  "#{rounded}% #{label}"
-end
-
 puts <<-MANIFESTO
 
 Defacer's goal is to be faster than any other javascript minification
@@ -25,6 +18,21 @@ are meeting that goal. The benchmarks are running now and will take a
 few seconds.
 
 MANIFESTO
+
+def percent_difference(baseline, measured, labels)
+  value = (measured.to_f - baseline.to_f) / baseline.to_f
+  label = (value < 0) ? labels.first : labels.last
+  rounded = (value.abs * 100).to_i
+  "#{rounded}% #{label}"
+end
+
+def benchmark_minifier(input, &block)
+  start_time = Time.now.to_f
+  minified = block.call(input)
+  gzipped_size = Zlib::Deflate.deflate(minified).size
+  elapsed = ((Time.now.to_f - start_time) * 1000).to_i
+  return gzipped_size, elapsed
+end
 
 defacer = Defacer
 closure = Closure::Compiler.new
@@ -49,23 +57,17 @@ Dir['spec/fixtures/benchmarks/*.js'].each do |js_file|
   row = []
   row << basename
 
-  closure_start_time = Time.now.to_f
-  closure_minified = closure.compile(input)
-  closure_gzipped_size = Zlib::Deflate.deflate(closure_minified).size
-  closure_elapsed = ((Time.now.to_f - closure_start_time) * 1000).to_i
+  closure_gzipped_size, closure_elapsed = benchmark_minifier(input) { |i| closure.compile(i) }
+  defacer_gzipped_size, defacer_elapsed = benchmark_minifier(input) { |i| defacer.compress(i) }
 
-  defacer_start_time = Time.now.to_f
-  defacer_minified = defacer.compress(input)
-  defacer_gzipped_size = Zlib::Deflate.deflate(defacer_minified).size
-  defacer_elapsed = ((Time.now.to_f - defacer_start_time) * 1000).to_i
-
-  row += [closure_gzipped_size,
-          defacer_gzipped_size,
-          percent_difference(closure_gzipped_size, defacer_gzipped_size, %w(smaller larger)),
-          closure_elapsed,
-          defacer_elapsed,
-          percent_difference(closure_elapsed, defacer_elapsed, %w(faster slower)),
-         ]
+  row += [
+    closure_gzipped_size,
+    defacer_gzipped_size,
+    percent_difference(closure_gzipped_size, defacer_gzipped_size, %w(smaller larger)),
+    closure_elapsed,
+    defacer_elapsed,
+    percent_difference(closure_elapsed, defacer_elapsed, %w(faster slower)),
+  ]
 
   # TODO show if we hit our goals or not
 
